4373c6d6e8e7b78102241fe329e47ed125496221
Enayet Hussain <anayetpr@gmail.com>
2026-06-07 22:57:47 +0600

    Update evaluateCustomPhoto.py

--- evaluateCustomPhoto.py
+++ evaluateCustomPhoto.py
@@ -1,6 +1,6 @@
 from custom_photo_evaluation import predict_gallery_photo
 
-MY_PHOTO = "trousers.jpg" 
+MY_PHOTO = "genji.jpg" 
 MY_SAVED_MODEL = "Fashion_MNIST_TorchModel.pth"
 
 predict_gallery_photo(MY_PHOTO, MY_SAVED_MODEL)
